21f8d8e8c7a00b35be95245ffd52928fbc28eeab
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-07 17:49:29 +0100

    create routes for technology admin

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\TypeController as GuestTypeController;
 use App\Http\Controllers\Admin\TypeController as AdminTypeController;
 use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Admin\TechnologyController as AdminTechnologyController;
 use App\Http\Controllers\Guest\ProjectController as GuestProjectController;
 use App\Http\Controllers\Guest\TechnologyController as GuestTechnologyController;
 
@@ -33,4 +34,5 @@ Auth::routes();
 Route::middleware('auth')->name('admin.')->prefix('admin')->group(function () {
     Route::resource('projects', AdminProjectController::class);
     Route::resource('types', AdminTypeController::class);
+    Route::resource('technologies', AdminTechnologyController::class);
 });
